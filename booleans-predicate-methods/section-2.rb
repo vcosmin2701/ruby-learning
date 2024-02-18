77862489bf56f3daf08600210d25f68815b65a38
vcosmin2701 <v.cosmin2717@gmail.com>
2024-02-18 22:56:30 +0200

    arithmetic methods

--- booleans-predicate-methods/section-2.rb
+++ booleans-predicate-methods/section-2.rb
@@ -12,3 +12,8 @@ puts "Porsche GT3 Touring".include?("P")
 # Methods with multiple arguments
 
 puts 20.between?(10, 27)
+
+# Arithmetic Methods
+puts 1 + 2 # 1.+(2) behind the scenes
+puts 1.+(2)
+puts 30.div(5) # alias for /

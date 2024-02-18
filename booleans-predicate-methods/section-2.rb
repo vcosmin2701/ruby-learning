d2d0ea647b3f151394909d30633240902aaf394d
vcosmin2701 <v.cosmin2717@gmail.com>
2024-02-18 23:02:33 +0200

    float methods

--- booleans-predicate-methods/section-2.rb
+++ booleans-predicate-methods/section-2.rb
@@ -17,3 +17,12 @@ puts 20.between?(10, 27)
 puts 1 + 2 # 1.+(2) behind the scenes
 puts 1.+(2)
 puts 30.div(5) # alias for /
+
+# Float Methods
+puts 16.43.floor # rounds down and return an integer
+puts 43.532.ceil # rounds up and return an integer
+puts 3.14159.round # rounds to the nearest integer
+puts 3.86.round.class # without precision
+puts 3.14159.round(2) # with precision and return a float
+
+puts -5.35.abs

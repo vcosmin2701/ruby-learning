90462d06404515b76ff81312c072d3840bf52338
vcosmin2701 <v.cosmin2717@gmail.com>
2024-02-19 23:37:39 +0200

    variables

--- ruby-variables/variables.rb
+++ ruby-variables/variables.rb
@@ -3,3 +3,11 @@ PI = 3.1415926
 puts PI
 PI = 3.14159265 # This will raise a warning
 puts PI
+
+# Variables
+car_brand = "Porsche"
+puts car_brand
+
+# Parallel assignment
+genre, artist = "Rock", "The Architects"
+puts "I'm listening to #{artist} and I like #{genre} music"

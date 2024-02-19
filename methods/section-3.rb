e422a9a472a49cb7cf0b428418cfa3732d7c31b0
vcosmin2701 <v.cosmin2717@gmail.com>
2024-02-19 22:31:31 +0200

    work with methods

--- methods/section-3.rb
+++ methods/section-3.rb
@@ -1,12 +1,13 @@
-# Method definition, without arguments
+# Method definition, without parameters
 def introduce_myself
   puts "I'm Cosmin, I'm a future software engineer"
 end
 
-def celebrate_birthday(name)
+def celebrate_birthday(name, age)
   puts "Happy Birthday, #{name}!"
+  puts "You are now #{age.succ} years old!"
 end
 
 # Call the method
 introduce_myself
-celebrate_birthday("Cosmin")
+celebrate_birthday("Cosmin", 22)

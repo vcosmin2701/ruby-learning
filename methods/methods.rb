84163ff043ac89dab8e726a3edb653a3be9ad0dc
vcosmin2701 <v.cosmin2717@gmail.com>
2024-02-20 23:24:17 +0200

    more on methods

--- methods/methods.rb
+++ methods/methods.rb
@@ -9,4 +9,33 @@ def multiply(val1, val2)
   puts result
 end
 
+# Passing arguments to a method
 multiply(2, 10)
+
+def display_strings(*args)
+  args.each {|string| puts string}
+end
+
+# Passing a variable number of arguments to a method
+display_strings("Red")
+display_strings("Red", "Blue", "Yellow")
+
+
+# Return statement
+def multiply(val1, val2)
+  result = val1 * val2
+  return result
+end
+
+value = multiply(10, 20)
+puts value
+
+# Method Aliases
+# def addition(val1, val2)
+#   result = val1 + val2
+#   puts result
+# end
+
+# alias do_add addition
+
+# do_add(5+5)

82a6d24dc3301687ccae361e816728f3053308d3
vcosmin2701 <v.cosmin2717@gmail.com>
2024-02-22 14:34:02 +0200

    intro hashes

---
+++ hashes/hash.rb
@@ -0,0 +1,10 @@
+# hash_variable = {"key1" => value1, "key2" => value2}
+empty_garage = {}
+
+geeks_default = Hash.new("GFG")
+puts "#{geeks_default[0]}"
+puts "#{geeks_default[7]}"
+
+languages = {"Ruby" => 1, "Java" => 2}
+puts languages.keys
+puts languages.values
